eb09ef1439f10c2473ebfedc9f1221e070813372
Spencer Woods <w.spencerj@gmail.com>
2018-12-31 14:57:05 -0700

    changes

--- index.php
+++ index.php
@@ -15,10 +15,10 @@ session_start();
 
 class test{
     public function signUp($user,$pass,$email){
-    $servername = "localhost:3306";
-    $username = "root";
-    $password = "pass";
-    $dbname = "sys";
+        $servername = "us-cdbr-iron-east-01.cleardb.net";
+        $username = "b3c9fea6937492";
+        $password = "fd9d1b2a";
+        $dbname = "heroku_e93bbdae1c33363";
 
     $conn =  new mysqli($servername,$username,$password,$dbname);
     if($conn->connect_error){
@@ -39,10 +39,10 @@ class test{
 
 
     public function login($user,$pass){
-        $servername = "localhost:3306";
-        $username = "root";
-        $password = "pass";
-        $dbname = "sys";
+        $servername = "us-cdbr-iron-east-01.cleardb.net";
+    $username = "b3c9fea6937492";
+    $password = "fd9d1b2a";
+    $dbname = "heroku_e93bbdae1c33363";
     
         $conn =  new mysqli($servername,$username,$password,$dbname);
         if($conn->connect_error){
