5621900b986dc7b804911453e6157b095a0fe7cf
Spencer Woods <w.spencerj@gmail.com>
2018-12-31 15:20:31 -0700

    took out echo when logging in

--- index.php
+++ index.php
@@ -90,8 +90,12 @@ if(isset($_POST['register'])){
 var log = '<?=$_SESSION['login'];?>';
 if(log == "1"){
     console.log("login is 1");
+    $("#loginLink").hide();
+    $("#nameLink").show();
 }
 else{
+    $("#loginLink").show();
+    $("#nameLink").hide();
     console.log("login is 0");
 }
 </script>
@@ -99,7 +103,8 @@ else{
 <body>
     <div class="container-fluid" id="bioContainer">
     <div class = "headerRow">
-        <a href="#" data-toggle="modal" data-target="#login-modal">Login</a>
+        <a href="#" data-toggle="modal" id = "loginLink" data-target="#login-modal">Login</a>
+        <a href="#" id = "nameLink">Name</a>
     </div>
 
     <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
